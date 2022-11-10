aa7ad82ca03b85019832202957cfeb3a19e906f3
bufleek <brunookiprop@gmail.com>
2022-11-10 08:33:55 +0300

    display model-loading indicator

--- app/src/main/java/com/arsample/ui/main/MainActivity.kt
+++ app/src/main/java/com/arsample/ui/main/MainActivity.kt
@@ -36,11 +36,14 @@ class MainActivity : AppCompatActivity(), Scene.OnUpdateListener {
      */
     private val loadingModels: ArrayList<String> = ArrayList()
     private val modelRenderables: HashMap<String, ModelRenderable> = HashMap()
+
     /**
      * Track loading detail views
      */
     private val loadingViews: ArrayList<String> = ArrayList()
     private val viewRenderables: HashMap<String, ViewRenderable> = HashMap()
+
+    private val loadingIndicatorRenderables: HashMap<String, ViewRenderable> = HashMap()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -83,25 +86,21 @@ class MainActivity : AppCompatActivity(), Scene.OnUpdateListener {
                             if (modelRenderable == null && !loadingModels.contains(artifact.model)) {
                                 // ensures that the model is not loading and has not been rendered
                                 val anchor = image.createAnchor(image.centerPose)
-                                renderArtifactModel(anchor, artifact)
+                                val loadingAnchor = image.createAnchor(getVerticalPose(image.centerPose))
+                                renderArtifactModel(anchor, artifact, loadingAnchor)
                             }
 
                             //render details card
                             val viewRenderable = viewRenderables[artifact.id.toString()]
-                            if(viewRenderable == null && !loadingViews.contains(artifact.id.toString())){
+                            if (viewRenderable == null && !loadingViews.contains(artifact.id.toString())) {
                                 val imageCenterPose = image.centerPose
                                 val anchor = image.createAnchor(
-                                    Pose(
-                                        floatArrayOf(
+                                    getVerticalPose(
+                                        imageCenterPose,
+                                        translation = floatArrayOf(
                                             imageCenterPose.translation[0],
-                                            imageCenterPose.translation[1]+ (image.extentX/2),
-                                            imageCenterPose.translation[2]
-                                        ),
-                                        floatArrayOf(
-                                            imageCenterPose.rotationQuaternion[0],
-                                            90f,
-                                            imageCenterPose.rotationQuaternion[2],
-                                            imageCenterPose.rotationQuaternion[3],
+                                            imageCenterPose.translation[1] + (image.extentX / 2),
+                                            imageCenterPose.translation[2],
                                         )
                                     )
                                 )
@@ -117,9 +116,24 @@ class MainActivity : AppCompatActivity(), Scene.OnUpdateListener {
     }
 
     /**
+     * returns a pose that is 90 degrees inclined to the face of the camera
+     */
+    private fun getVerticalPose(pose: Pose, translation: FloatArray? = null): Pose {
+        return Pose(
+            translation ?: pose.translation,
+            floatArrayOf(
+                pose.rotationQuaternion[0],
+                90f,
+                pose.rotationQuaternion[2],
+                pose.rotationQuaternion[3],
+            )
+        )
+    }
+
+    /**
      * displays the artifacts detail card
      */
-    private fun renderArtifactDetailView(artifact: Artifact, anchor: Anchor){
+    private fun renderArtifactDetailView(artifact: Artifact, anchor: Anchor) {
         Log.d(TAG, "renderArtifactDetailView: rendering ${artifact.name}")
         loadingViews.add(artifact.id.toString())
         ViewRenderable.builder()
@@ -132,7 +146,8 @@ class MainActivity : AppCompatActivity(), Scene.OnUpdateListener {
 
                 val view = it.view
                 view.findViewById<TextView>(R.id.tv_artifact_title).text = artifact.name
-                view.findViewById<TextView>(R.id.tv_artifact_description).text = artifact.description
+                view.findViewById<TextView>(R.id.tv_artifact_description).text =
+                    artifact.description
 
                 placeArtifactDetailView(it, anchor, artifact)
                 loadingViews.removeIf { lv ->
@@ -154,18 +169,47 @@ class MainActivity : AppCompatActivity(), Scene.OnUpdateListener {
         viewRenderable: ViewRenderable,
         anchor: Anchor,
         artifact: Artifact
-    ){
+    ) {
         val anchorNode = AnchorNode(anchor)
         anchorNode.renderable = viewRenderable
         arSceneFragment.arSceneView.scene.addChild(anchorNode)
         viewRenderables[artifact.id.toString()] = viewRenderable
     }
 
+    private fun showModelLoadingIndicator(artifact: Artifact, anchor: Anchor) {
+        ViewRenderable.builder()
+            .setView(this, R.layout.artifact_model_loading_indicator)
+            .build().thenAccept {
+                it.isShadowCaster = false
+                it.isShadowReceiver = false
+                it.sizer = FixedWidthViewSizer(0.08f)
+                val anchorNode = AnchorNode(anchor)
+                anchorNode.renderable = it
+                arSceneFragment.arSceneView.scene.addChild(anchorNode)
+                loadingIndicatorRenderables[artifact.model] = it
+            }
+    }
+
+    private fun dismissModelLoadingIndicator(artifact: Artifact) {
+        val loadingRenderable = loadingIndicatorRenderables[artifact.model]
+        if (loadingRenderable != null) {
+            val child = arSceneFragment.arSceneView.scene.children.firstOrNull {
+                it.renderable == loadingRenderable
+            }
+            if (child != null) {
+                arSceneFragment.arSceneView.scene.removeChild(child)
+            }
+        }
+    }
+
     /**
      * Loads the artifact's 3d model
      */
-    private fun renderArtifactModel(anchor: Anchor, artifact: Artifact) {
+    private fun renderArtifactModel(anchor: Anchor, artifact: Artifact, loadingAnchor: Anchor) {
         loadingModels.add(artifact.model)
+
+        showModelLoadingIndicator(artifact, loadingAnchor)
+
         ModelRenderable.builder()
             .setSource(
                 this, RenderableSource.builder().setSource(
@@ -179,6 +223,7 @@ class MainActivity : AppCompatActivity(), Scene.OnUpdateListener {
             .setRegistryId(artifact.model)
             .build()
             .thenAccept {
+                dismissModelLoadingIndicator(artifact)
                 it.isShadowReceiver = false
                 it.isShadowCaster = false
                 placeModel(it, anchor)
@@ -190,6 +235,7 @@ class MainActivity : AppCompatActivity(), Scene.OnUpdateListener {
             }
             .exceptionally {
                 Log.d(TAG, "createAnchor: Unable to load renderable $it")
+                dismissModelLoadingIndicator(artifact)
                 loadingModels.removeIf { model ->
                     model == artifact.model
                 }
